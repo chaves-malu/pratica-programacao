c1f122ba71e5e47c8608705f02c4b85ab9b741ce
chaves-malu <106176998+chaves-malu@users.noreply.github.com>
2022-06-01 07:55:10 -0500

    Criei uma nova pasta para ficar mais organizado

---
+++ site_teste/enviar.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = $_REQUEST['nome'];
+$genero = $_REQUEST['genero'];
+$estado = $_REQUEST['estado'];
+$qtdIrmaos = $_REQUEST['irmao'];
+
+foreach ($qtdIrmaos as $val)
+{
+    $msg .= $val . ", ";
+}
+
+echo "O nome é $nome,oi o gênero é $genero, o estado é $estado e são $msg irmãos.";
+
+?>

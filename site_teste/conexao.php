1b307b3f586caf9ff9ce41043ba666125d7ca064
chaves-malu <106176998+chaves-malu@users.noreply.github.com>
2022-06-01 08:24:32 -0500

    banco

--- site_teste/conexao.php
+++ site_teste/conexao.php
@@ -1,5 +1,11 @@
 <?php
 
-    $conexao = mysqli_connect('localhost', 'cadastro', 'root', '');
+    $conexao = mysqli_connect('localhost', 'root', '', 'cadastro');
+
+    if (!$conexao)
+    {
+        die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
+    }
+    
 
 ?>

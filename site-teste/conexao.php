235dd9c8681aa6193a4fa06f3e74ac056ccd60de
chaves-malu <106176998+chaves-malu@users.noreply.github.com>
2022-06-08 08:26:43 -0500

    chaves

--- site-teste/conexao.php
+++ site-teste/conexao.php
@@ -8,8 +8,10 @@
     $conexao = mysqli_connect($porta, $usuario, $senha, $banco);
 
     if (!$conexao)
+    {
         die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
-    
+    }
+
     echo "<br>Conexão realizada com sucesso!";
 
 ?>

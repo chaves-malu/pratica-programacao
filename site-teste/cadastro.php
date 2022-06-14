70036ba1ea75bde9a87202ba4fb845bc831b1b9b
chaves-malu <106176998+chaves-malu@users.noreply.github.com>
2022-06-14 12:20:59 -0500

    mudei muita coisa

--- site-teste/cadastro.php
+++ site-teste/cadastro.php
@@ -1,5 +1,19 @@
 <?php
 
-    include (conexao.php);
+    include ('conexao.php');
+
+    $nome = $_REQUEST['name'];
+    $nascimento = $_REQUEST['date'];
+    $email = $_REQUEST['email'];
+    $senha = $_REQUEST['password'];
+
+    $sql = "INSERT INTO usuarios ('nome', 'nascimento', 'email', 'senha')  VALUES ('$nome', '$nascimento', '$email', '$senha')";
+    
+    if (mysqli_query($conexao, $sql)) 
+        echo "Cadastro realizado com sucesso!";
+    else 
+        echo "Erro! ". mysqli_connect_errno($conexao);
+    
+    mysqli_close($conexao);
 
 ?>
